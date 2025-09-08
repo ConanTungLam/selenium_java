7c82028ea6ff9a44a0bf2b8be212aacbd8fd36b0
Conan <conan@192.168.1.104.non-exists.ptr.local>
2025-09-08 23:34:37 +0700

    day 3

---
+++ src/test/java/heroku/CheckboxTest.java
@@ -0,0 +1,46 @@
+package heroku;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CheckboxTest {
+    @Test
+    void checkedChecbox(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+//        driver.findElement(By.xpath("//form[@id='checkboxes']/input[1]")).click();
+//        Assert.assertTrue(driver.findElement(By.xpath("//form[@id='checkboxes']/input[1]")).isSelected());
+//        driver.findElement(By.xpath("//form[@id='checkboxes']/input[2]")).click();
+//        Assert.assertTrue(driver.findElement(By.xpath("//form[@id='checkboxes']/input[2]")).isSelected());
+        WebElement checkbox1 = driver.findElement(By.xpath("//form[@id='checkboxes']/input[1]"));
+        WebElement checkbox2 = driver.findElement(By.xpath("//form[@id='checkboxes']/input[2]"));
+
+        if (!checkbox1.isSelected()) checkbox1.click();
+        if (!checkbox2.isSelected()) checkbox2.click();
+
+        Assert.assertTrue(checkbox1.isSelected());
+        Assert.assertTrue(checkbox2.isSelected());
+        driver.quit();
+    }
+
+    @Test
+    void uncheckedChecbox(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+
+        WebElement checkbox1 = driver.findElement(By.xpath("//form[@id='checkboxes']/input[1]"));
+        WebElement checkbox2 = driver.findElement(By.xpath("//form[@id='checkboxes']/input[2]"));
+
+        if (checkbox1.isSelected()) checkbox1.click();
+        if (checkbox2.isSelected()) checkbox2.click();
+
+        Assert.assertFalse(checkbox1.isSelected());
+        Assert.assertFalse(checkbox2.isSelected());
+        driver.quit();
+    }
+
+}
---
+++ src/test/java/heroku/DemoLoginTest.java
@@ -0,0 +1,31 @@
+package heroku;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoLogin {
+    @Test
+    void loginWithValidAccount(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html");
+        driver.quit();
+    }
+    @Test
+    void loginWithInvalidPassword(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce001");
+        driver.findElement(By.id("login-button")).click();
+        String invalidMessage = driver.findElement(By.className("error")).getText();
+        Assert.assertTrue(invalidMessage.contains("Epic sadface: Username and password do not match any user in this service"));
+        driver.quit();
+    }
+}
